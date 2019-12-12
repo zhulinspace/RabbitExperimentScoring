0523b0b535506407606e62457e1cba5ba080d927
zhulispace <295042027@qq.com>
2019-12-12 17:22:52 +0800

    修改checkobject

--- utils/Detector.py
+++ utils/Detector.py
@@ -1,116 +1,105 @@
 import cv2
-import glob
-import numpy
-
-from utils.GiveMark import GradeSYS
-from utils.StageEstimate import StageEstimate
-from utils.time_clock import clock
+import time
+import numpy as np
+# import GiveMark
+# from StageEstimate import StageEstimate
 
 
 class Detector:
     # 运行的检测类，用于检测当前图片上的物品，并维护一个 List checkedObject
 
-    def __init__(self,deploy_path, model_path, jason_file_path):
-        self._deploy_path = deploy_path
-        self._model_path = model_path
-        self._jason_file_path = jason_file_path
-        self._size = 300
-        self._classNum = 6  # 目前能够识别的物品数量
-        # 列表的值 第一项为 Object 到目前为止出现的次数(为了减小错误识别造成的影响，暂定出现40次时确定该物品已被检测到) , 第二项为一个标识 Object 当前位置的元组
-        self._checkedObjects = [[0, 0, []] for i in range(self._classNum)]  # 当前帧是否有目标，目标出现次数，目标坐标
-        self._checkedObjects.append([-1])
-        self._net = cv2.dnn.readNetFromCaffe(self._deploy_path, self._model_path)
-        self._stage_estimating = StageEstimate(self._jason_file_path)
-        self._stage_estimating.create_graph()
-        self._clock = clock()
+    def __init__(self):
+        self.deploy_path = 'no_bn.prototxt'
+        self.model_path = 'no_bn.caffemodel'
+        self.SIZE = 300
+        self.classNum = 6  # 目前能够识别的物品数量
+        # 列表的值 第一项为 Object 到目前为止出现的次数(为了减小错误识别造成的影响，暂定出现40次时确定该物品已被检测到) , 第二项为一个标识 Object 当前位置的元组\
+        res = np.empty(shape=[0, 4], dtype=int)
+        self.checkedObjects = [[0, 0, res] for i in range(self.classNum)]  # 当前帧是否有目标，目标出现次数，目标坐标；二维列表每一个元素代表一个对象
+        # self.checkedObjects.append([-1])  # checkedObjects[6][0]代表阶段
+        self.net = cv2.dnn.readNetFromCaffe(self.deploy_path, self.model_path)
+        # self.se= StageEstimate("ScoresLine.json")
+        # self.se.create_graph()
 
-    @property
-    def size(self):
-        return self._size
+    def getCheckedObjects(self):
+        return self.checkedObjects
 
-    @size.setter
-    def size(self, new_size):
-        self._size = new_size
-
-    @property
-    def checkedObjects(self):
-        return self._checkedObjects
-
-    @checkedObjects.setter
-    def checkedObjects(self, new_checked_objects):
-        self._checkedObjects = new_checked_objects
-
-    def print_checked_objects(self):
-        obj_name = ['兔子', '剪刀', '伤口', '手', '耳朵', '针头']
-        checked_obj = self._checkedObjects[:6]
-        stage_name = ['抓拿', '麻醉', '固定', '手术']
-        now_stage = stage_name[self._checkedObjects[6][0]]
-        print("Stage :", now_stage)
-        for obj_name, obj in zip(obj_name, checked_obj):
-            print("【", obj_name, "】", ": ", obj[0], " ", obj[1], obj[2], end=" ")
+    def printCheckedObjects(self):
+        objName = ['兔子', '剪刀', '伤口', '手', '耳朵', '针头']
+        stageName = ['抓拿', '麻醉', '固定', '手术']
+        # print("Stage :", stageName[self.checkedObjects[6][0]])
+        for oName, obj in zip(objName, self.checkedObjects):
+            print("【", oName, "】", ": ", obj[0], " ", obj[1], obj[2], end=" ")
         print()
 
-    def reset_obj(self):
-        for obj in self._checkedObjects:
+    def checkImg(self, img):
+        assert isinstance(img, np.ndarray), "输入对象不是图片!"
+        for obj in self.checkedObjects:
             if len(obj) > 1:
                 obj[0] = 0  # 重置目标是否出现
 
-    def set_net_input(self, img):
-        self._net.setInput(cv2.dnn.blobFromImage(img,
-                                                 1.0 / 127.5,
-                                                 (300, 300),
-                                                 (127.5, 127.5, 127.5),
-                                                 swapRB=False, crop=False))
-
-    def check_img(self, img):
-        assert (isinstance(img, numpy.ndarray)), "输入对象不是图片!"
-        self.reset_obj()
-
         # 1.Check Stage
-        self._checkedObjects[6][0] = self._stage_estimating.estimate(img)[0]
+        # stageEs = self.se.estimate(img)
+        # self.checkedObjects[6][0] = stageEs[0]
 
         # 2. Find and rectangle object
-        img_w, img_h, *_ = img.shape
-        self.set_net_input(img)
-        self._clock.tic()
-        cv_out = self._net.forward()
-        print('time cost: ', self._clock.toc())
+        rows = img.shape[0]
+        cols = img.shape[1]
+        # cv2.dnn.blobFromImage
+        # 对图像进行归一化操作(-1, 1)，参数说明：img表示输入图片，0.007843 表示需要乘的数，即1 / 127.5，(w, h) 表示图像大小，127.5表示需要减去的数
+        # setInput表示将图片输入到caffe网络中
+        self.net.setInput(
+            cv2.dnn.blobFromImage(img, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=False, crop=False))
 
-        for detection in cv_out[0, 0, :, :]:
-            """
-            Detection: shape is (1, 7), which is (response, class, score, coords).
-            """
+        start_time = time.time()
+        cvOut = self.net.forward()
+        print('time cost: ', time.time() - start_time)
+
+        # 清零每一帧的bounding box
+        for i in range(self.classNum):
+            self.checkedObjects[i][2] = np.empty(shape=[0, 4], dtype=int)
+
+        for detection in cvOut[0, 0, :, :]:
             score = float(detection[2])
             if score > 0.3:
-                left = detection[3] * img_h
-                top = detection[4] * img_w
-                right = detection[5] * img_h
-                bottom = detection[6] * img_w
-                self._checkedObjects[int(detection[1] - 1)][0] = 1
-                self._checkedObjects[int(detection[1] - 1)][1] += 1
-                self._checkedObjects[int(detection[1] - 1)][2] = [int(left), int(top), int(right), int(bottom)]
+                left = detection[3] * cols
+                top = detection[4] * rows
+                right = detection[5] * cols
+                bottom = detection[6] * rows
+                cls = int(detection[1] - 1)
+                self.checkedObjects[cls][0] = 1
+                self.checkedObjects[cls][1] += 1
+                boudingbox = [int(left), int(top), int(right), int(bottom)]
+                self.checkedObjects[cls][2] = np.append(self.checkedObjects[cls][2], [boudingbox], axis=0)
+                # self.checkedObjects[int(detection[1] - 1)][2].append (int(left), int(top), int(right), int(bottom))
 
                 cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
                 text = str(detection[1])
                 cv2.putText(img, text, (int(left), int(top)), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 0), 1)
 
-        img = cv2.resize(img, (int(img_h / 2), int(img_w / 2)))
+        ratiao = rows / cols
+        img = cv2.resize(img, (int(cols / 2), int(cols / 2 * ratiao)))
         return img
 
 
 # 测试
 if __name__ == '__main__':
-    jason_file_path = '../ScoresLine.json'
-    detector = Detector(jason_file_path)
-    grade_sys = GradeSYS(jason_file_path)
-    imgStream = glob.glob("../img_catching/*.png")  # 获取img路径下的所有 jpg 图片
-    for im in imgStream:
+    detector = Detector()
+    gradSys = GiveMark.GradeSYS("ScoresLine.json")
+
+    import glob
+
+    imgStram = glob.glob("img_neddle/*.jpg")  # 获取img路径下的所有 jpg 图片
+    for im in imgStram:
         im = cv2.imread(im)
-        im = detector.check_img(im)
-        detector.print_checked_objects()
-        grade_sys.begin_mark_line(detector.checkedObjects)
-        grade_sys.print_transcript()
+        im = detector.checkImg(im)
+        detector.printCheckedObjects()
+        res = detector.getCheckedObjects()
+        # gradSys.beginMarkLine(detector.getCheckedObjects())
+        # gradSys.printTranscript()
         cv2.imshow("img", im)
-        k = cv2.waitKey(0)
-        if k == ord('q'):
+        if cv2.waitKey(0) == 27:
             break
+
+
+
